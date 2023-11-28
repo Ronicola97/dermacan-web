d3d7f2bb447a68d92aa6bae4b9da33c877d4a388
Ronicola97 <120481678+Ronicola97@users.noreply.github.com>
2023-11-28 14:41:40 -0500

    fix guardar mascota

--- guardar_mascota.php
+++ guardar_mascota.php
@@ -8,19 +8,20 @@ $n_prop = $_POST['nombre_prop'];
 $direc = $_POST['direccion'];
 $tipo_raza = $_POST['raza'];
 $date = $_POST['fecha'];
+$estado = 1;
 
 if ($numerodefilas > 0) {
     $existe = "existe";
     echo $existe;
 }else{
-$sentencia = "INSERT INTO mascota(pet_name, pet_dir, pet_nacimiento, 
-            pet_estado, raza_id, usu_cedula) 
+$sentencia = "INSERT INTO mascota(nombre_pet, direccion_pet, fnaci_pet, 
+            pet, estado_pet, raza_id, cedula_usu) 
 		VALUES ('$n_perro','$direc', '$date','1', '$tipo_raza', '$n_prop')";
 $respuesta = $conn->query($sentencia);
 }
 
 if($respuesta==true){
-	echo 'Se ha registrado con éxito';
+	echo 'grabado';
 }else{
 	echo 'Se ha producido un error';
 }
