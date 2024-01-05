d235fda81b66967b1499b0aa14cc91215b838a0b
Ronicola97 <loornico15@gmail.com>
2024-01-04 21:34:30 -0500

    test image save

--- android/guardar_mascota.php
+++ android/guardar_mascota.php
@@ -10,18 +10,30 @@ $tipo_raza = $_POST['tipo_raza'];
 $date = $_POST['nacimiento'];
 $estado = 1;
 
+// Obtener la imagen codificada en Base64 desde el POST
+$imagen_base64 = $_POST['imagen_mascota'];
+
+// Decodificar la imagen Base64
+$imagen_binaria = base64_decode($imagen_base64);
+
+// Guardar la imagen en una ruta específica en tu servidor (por ejemplo, en una carpeta llamada 'imagenes')
+$ruta_imagen = '../imagenes/' . uniqid() . '.jpg';  // Aquí estás generando un nombre único para la imagen
+$resultado_guardado = file_put_contents($ruta_imagen, $imagen_binaria);
 
 
+if ($resultado_guardado !== false) {
+    // Insertar los datos de la mascota en la base de datos
+    $sentencia = "INSERT INTO mascota(nombre_pet, direccion_pet, fnaci_pet, raz_pet, cedula_usu, estado_pet, ruta_imagen) 
+                    VALUES ('$n_perro', '$direc', '$date', '$tipo_raza', '$n_prop', '$estado', '$ruta_imagen')";
+    $respuesta = $conn->query($sentencia);
 
-$sentencia = "INSERT INTO mascota(nombre_pet, direccion_pet, fnaci_pet, raz_pet, cedula_usu,estado_pet) 
-		VALUES ('$n_perro','$direc', '$date', '$tipo_raza', '$n_prop','$estado')";
-$respuesta = $conn->query($sentencia);
-
-
-if($respuesta==true){
-	echo 'grabado';
-}else{
-	echo 'Se ha producido un error';
+    if ($respuesta === true) {
+        echo 'grabado';
+    } else {
+        echo 'Se ha producido un error al insertar en la base de datos';
+    }
+} else {
+    echo 'Se ha producido un error al guardar la imagen';
 }
 $conn->close();
 
