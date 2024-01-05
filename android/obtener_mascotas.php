7e9db1c5495d8e47d3d5385a0b4a461573e32461
Ronicola97 <loornico15@gmail.com>
2024-01-05 00:36:48 -0500

    test get img

--- android/obtener_mascotas.php
+++ android/obtener_mascotas.php
@@ -13,9 +13,24 @@ $sql = "select * from mascota where cedula_usu='$dueno'";
 $respuesta = $conn->query($sql);
 $no_filas = $respuesta->num_rows;
 if($no_filas>0){
-	while($filas = $respuesta->fetch_assoc()){
-		array_push($arreglo, $filas);
-	}
+	while ($filas = $respuesta->fetch_assoc()) {
+        // Asumiendo que la columna 'url_imagen' almacena la URL de la imagen en GCS
+        $imagen_url = $filas['ruta_imagen'];
+        
+        // Si deseas agregar más campos o modificar la estructura de los datos, puedes hacerlo aquí
+        $mascota_data = array(
+            'id_pet' => $filas['id_pet'],
+            'nombre_pet' => $filas['nombre_pet'],
+            'direccion_pet' => $filas['direccion_pet'],
+            'fnaci_pet' => $filas['fnaci_pet'],
+            'raz_pet' => $filas['raz_pet'],
+            'cedula_usu' => $filas['cedula_usu'],
+            'estado_pet' => $filas['estado_pet'],
+            'ruta_imagen' => $imagen_url // Agregamos la URL de la imagen desde GCS
+        );
+        
+        array_push($arreglo, $mascota_data);
+    }
 	echo json_encode($arreglo,JSON_UNESCAPED_UNICODE);
 }
 else{
