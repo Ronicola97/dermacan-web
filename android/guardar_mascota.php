492bdd05245b4f62b96d686d5364d4ef3fbbbcae
Ronicola97 <loornico15@gmail.com>
2024-01-04 23:50:32 -0500

    test save img

--- android/guardar_mascota.php
+++ android/guardar_mascota.php
@@ -10,36 +10,46 @@ $tipo_raza = $_POST['tipo_raza'];
 $date = $_POST['nacimiento'];
 $estado = 1;
 
-// Obtener la imagen codificada en Base64 desde el POST
+// Decodificar la imagen en Base64
 $imagen_base64 = $_POST['imagen_mascota'];
+$imagen_decodificada = base64_decode($imagen_base64);
 
-// Decodificar la imagen Base64
-$imagen_binaria = base64_decode($imagen_base64);
+// Configurar la conexión con Google Cloud Storage
+require 'vendor/autoload.php';
+use Google\Cloud\Storage\StorageClient;
 
-// Verifica si el directorio no existe y créalo si es necesario
-if (!is_dir('../imagenes')) {
-    mkdir('../imagenes', 0777, true); // Esto crea el directorio con permisos de lectura, escritura y ejecución para todos
-}
+$storage = new StorageClient([
+    'keyFilePath' => 'adept-portal-397013-1d8a23b30297.json', // Ruta a tu archivo de credenciales
+    'projectId' => 'adept-portal-397013',
+]);
+$bucketName = 'dermacan-storage';
 
-// Guardar la imagen en una ruta específica en tu servidor (por ejemplo, en una carpeta llamada 'imagenes')
-$ruta_imagen = '../imagenes/' . uniqid() . '.jpg';  // Aquí estás generando un nombre único para la imagen
-$resultado_guardado = file_put_contents($ruta_imagen, $imagen_binaria);
+// Subir imagen a GCS
+$temp_file = tempnam(sys_get_temp_dir(), 'gcs_temp_file');
+file_put_contents($temp_file, $imagen_decodificada);
 
+$bucket = $storage->bucket($bucketName);
+$object = $bucket->upload(
+    fopen($temp_file, 'r'),
+    [
+        'name' => uniqid().'.jpg' // Nombre único para el archivo
+    ]
+);
+$imageUrl = $object->info()['mediaLink']; // Obtener la URL de la imagen en GCS
 
-if ($resultado_guardado !== false) {
-    // Insertar los datos de la mascota en la base de datos
-    $sentencia = "INSERT INTO mascota(nombre_pet, direccion_pet, fnaci_pet, raz_pet, cedula_usu, estado_pet, ruta_imagen) 
-                    VALUES ('$n_perro', '$direc', '$date', '$tipo_raza', '$n_prop', '$estado', '$ruta_imagen')";
-    $respuesta = $conn->query($sentencia);
+// Insertar datos en la base de datos
+$sentencia = "INSERT INTO mascota(nombre_pet, direccion_pet, fnaci_pet, raz_pet, cedula_usu, estado_pet, ruta_imagen) 
+        VALUES ('$n_perro','$direc', '$date', '$tipo_raza', '$n_prop', '$estado', '$imageUrl')";
+$respuesta = $conn->query($sentencia);
 
-    if ($respuesta === true) {
-        echo 'grabado';
-    } else {
-        echo 'Se ha producido un error al insertar en la base de datos';
-    }
+if ($respuesta == true) {
+    echo 'grabado';
 } else {
-    echo 'Se ha producido un error al guardar la imagen';
+    echo 'Se ha producido un error';
 }
-$conn->close();
 
+// Eliminar el archivo temporal
+unlink($temp_file);
+
+$conn->close();
 ?>
