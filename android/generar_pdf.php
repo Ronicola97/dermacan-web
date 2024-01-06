e09a4cbb9c51e5ecd5642a18c07f52ddff8a6733
Ronicola97 <loornico15@gmail.com>
2024-01-05 22:44:56 -0500

    test pdf

--- android/generar_pdf.php
+++ android/generar_pdf.php
@@ -1,5 +1,6 @@
 <?php
 include("../db/conexion.php");
+require('fpdf/fpdf.php');
 
 date_default_timezone_set("America/Guayaquil");
 
@@ -8,7 +9,35 @@ $id_pet = $_POST['id_pet'];
 $id_fcder = $_POST['id_fcder'];
 $id_diag = $_POST['id_diag'];
 
-echo $id_pet.$id_fcder.$id_diag;
+
+$sql = "select * 
+from ficha_dermatologica fd, diagnostico d, mascota m 
+where (
+fd.id_fcder = '$id_fcder' and
+m.id_pet = fd.id_mas and fd.id_fcder = d.id_fcder and u.cedula_usu = m.cedula_usu
+);";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0){
+
+    // Crear instancia de FPDF
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial', 'B', 16);
+
+    $pdf->Cell(0, 10, 'Historial Médico', 0, 1, 'C');
+
+    while ($row = $result->fetch_assoc()) {
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(0, 10, 'Nombre Mascota: ' . $row['nombre_pet'], 0, 1);
+        $pdf->Cell(0, 10, 'Fecha: ' . $row['fecha_fcder'], 0, 1);
+        $pdf->Cell(0, 10, 'Diagnóstico: ' . $row['enf_diag'], 0, 1);
+    }
+
+    $pdf->Output();
+
+    
+}
 
 
 $conn->close();
